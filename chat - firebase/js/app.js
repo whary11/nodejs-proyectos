88ccce852f54f550b56e3b6ba97a2cebf646d09c
whary11 <whary11@gmail.com>
2017-11-26 18:23:47 -0500

    Pasando todo el código al paradigma Funcional

--- chat - firebase/js/app.js
+++ chat - firebase/js/app.js
@@ -5,6 +5,10 @@ jQuery(document).ready(function($) {
 		nombre: 'Luis Fernando Raga Renteria',
 		mensaje: 'Hola hola'
 	}
+	var usuario = {
+		id:1,
+		nombre:"Luis Raga"
+	}
 	var dialog = document.querySelector('dialog');
     var showDialogButton = document.querySelector('#show-dialog');
     if (! dialog.showModal) {
@@ -13,26 +17,36 @@ jQuery(document).ready(function($) {
     	dialog.showModal();
     }
     $('#empezar').click(function(event) {
-
-
     	if (!vacio($('#nombre').val())) {
+    		
 	    	var snackbarContainer = document.querySelector('#demo-toast-example');
-		    var message = {message: 'Debes escribir un nombre o NickName'};
+		    var message = {message: 'Debes escribir un Nombre o NickName'};
 		    snackbarContainer.MaterialSnackbar.showSnackbar(message);
     	}else{
+    		// Agragamos el nombre ingresado al JSON de data 
     		data.nombre = $('#nombre').val();
-	    	console.log(data)
+    		usuario.nombre = $('#nombre').val();
+    		// Cerramos laventana de dialogo.
+
 	    	dialog.close();
+	    	renderUsers(usuario, "#usuarios")
     	}
     });
-	
-
 	// Adiciona todos los colores que quieras y que soporte la librería de mdl
-	var colores = ['black', 'blue', 'green', 'lime', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'yellow'];
-	
+	var colores = ['black',
+					'blue',
+					'green',
+					'lime',
+					'orange',
+					'purple',
+					'red',
+					'teal',
+					'yellow'
+					];
+	data.color = color(colores);
+	usuario.color = data.color;
 	$(".envio").click(() => {
 		var texto = $('#texto-mensaje').val();
-
 		if (!vacio(texto)) {
 			// Tomar acción si el campo está vacio o contiene sólo espacios en blanco.
 			var snackbarContainer = document.querySelector('#demo-toast-example');
@@ -41,7 +55,7 @@ jQuery(document).ready(function($) {
 		}else{
 			//Tomar accion si el texto esta conrrecto y se puede enviar a los demas usuarios conectados.
 			data.mensaje = texto;
-			render(data, colores);
+			renderMensaje(data, "#mensajes");
 			$('#texto-mensaje').val('');	
 		}
 
@@ -58,25 +72,69 @@ jQuery(document).ready(function($) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ////////////////// Funciones ////////////
 // Función para renderizarlos mensajes
-function render(data, colores){
-	function color(){
-		return colores[Math.floor(Math.random() * (colores.length - 0)) + 0];
-	}
+function renderMensaje(data, id){
 	var html = `
-		<div>
-            <span class="mdl-chip mdl-chip--contact right">
-                <span class="mdl-chip__contact mdl-color--${color()} mdl-color-text--white">${PL(data.nombre)}</span>
-                <span class="mdl-chip__text">${data.mensaje}</span>
+		<div id="tt1">
+            <span class="mdl-chip mdl-chip--contact">
+                <span class="mdl-chip__contact mdl-color--${data.color} mdl-color-text--white">${PL(data.nombre)}</span>
+                <span class="mdl-chip__text">${data.nombre}</span>
             </span>
         </div>
-        <br>
+        <span class="mdl-tooltip" for="tt1">${data.mensaje}</span>
 	`;
-
-	$('#mensajes').append(html).fadeTo()
+	$(id).append(html);
 }
-
+// Función para renderizarlos usuarios
+function renderUsers(user, id){
+	var html = `
+		<div>
+            <span class="mdl-chip mdl-chip--contact">
+                <span class="mdl-chip__contact mdl-color--${user.color} mdl-color-text--white">${PL(user.nombre)}</span>
+                <span class="mdl-chip__text">${user.nombre}</span>
+            </span>
+        </div>`;
+	$(id).append(html);
+}
 // Función para extraer la primera letra de un string
 function PL (string) {
 	for(var i = 0; i==0;i++){
@@ -84,9 +142,11 @@ function PL (string) {
 	}
 }
 
+// Función para seleccionar el color de forma automatica 
 
-
-
+function color(colores){
+		return colores[Math.floor(Math.random() * (colores.length - 0)) + 0];
+	}
 
 // Función que valida que el campo no sea sólo espacios
 function vacio(q) {
