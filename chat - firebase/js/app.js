f2dcfd693bb3b8f770e5cf993d420dcc1e94377d
whary11 <whary11@gmail.com>
2017-12-02 09:04:56 -0500

    Se realizaron las funciones y eventos para detectar cuando se envían mensajes y trasmitirlos entre usuariios .

--- chat - firebase/js/app.js
+++ chat - firebase/js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 jQuery(document).ready(function($) {
+
+	// Variable de inicio
 	var conteo =0;
 	var data = {
 		id:1,
@@ -12,23 +14,16 @@ jQuery(document).ready(function($) {
 		correo: "",
 		imagen: ""
 	}
-
-	
-	var userGoogle = googleAuth(usuario);
+	var baseDatos = firebase.database();
 
 
 
+
+	var userGoogle = googleAuth(usuario, baseDatos);
 	// Adiciona todos los colores que quieras y que soporte la librería de mdl
-	var colores = ['black',
-					'blue',
-					'green',
-					'lime',
-					'orange',
-					'purple',
-					'red',
-					'teal',
-					'yellow'
-					];
+	var colores = ['black','blue','green','lime','orange','purple','red','teal','yellow'];
+
+
 	data.color = color(colores);
 	usuario.color = data.color;
 
@@ -36,71 +31,10 @@ jQuery(document).ready(function($) {
 
 
 
-
-	$(".envio").click(() => {
-		var texto = $('#texto-mensaje').val();
-		if (!vacio(texto)) {
-			// Tomar acción si el campo está vacio o contiene sólo espacios en blanco.
-			var snackbarContainer = document.querySelector('#demo-toast-example');
-		    var message = {message: 'Debes escribir un mensaje'};
-		    snackbarContainer.MaterialSnackbar.showSnackbar(message);
-		}else{
-			//Tomar accion si el texto esta conrrecto y se puede enviar a los demas usuarios conectados.
-			data.mensaje = texto;
-			// console.log(data);
-			var mensaje = {
-				idUser: usuario.id,
-				nombre: usuario.nombre,
-				mensaje: texto
-			}
-			console.log(mensaje);
-			nuevoMensaje(mensaje);
-			renderMensaje(data, "#mensajes");
-			$('#texto-mensaje').val('');
-
-
-		}
-	})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	agregarMensaje(baseDatos);
+ // Eventos
+ 	baseDatos.ref("/mensajes").on('child_added', trasmitrirMsj);
+ 	baseDatos.ref("usuarios").on('child_added', trasmitrirUsuario)
 
 
 
@@ -115,19 +49,87 @@ jQuery(document).ready(function($) {
 
 
 ////////////////// Funciones ////////////
+// Función para trasmitir los mensajes a todos los usuarios conectados
+
+function trasmitrirMsj(data) {
+	if (usuario.id!=data.val().idUser) {
+		renderMensaje(data.val(), "#mensajes");
+
+	}
+	// console.log(data.val());
+}
+// Función para tlistar los usuarios conectados
+function trasmitrirUsuario(data){
+	if (usuario.id!=data.val().id) {
+		renderUsers(data.val(), "#usuarios")
+		// Notificar la nueva conexión
+		$('#numconect').attr('data-badge', conteo++);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Función para agregar mensajes en el FronEnd y Backedn
+	function agregarMensaje(db){
+		$(".envio").click(() => {
+			var texto = $('#texto-mensaje').val();
+			if (!vacio(texto)) {
+				// Tomar acción si el campo está vacio o contiene sólo espacios en blanco.
+				var snackbarContainer = document.querySelector('#demo-toast-example');
+			    var message = {message: 'Debes escribir un mensaje'};
+			    snackbarContainer.MaterialSnackbar.showSnackbar(message);
+			}else{
+				//Tomar accion si el texto esta conrrecto y se puede enviar a los demas usuarios conectados.
+				data.mensaje = texto;
+				// console.log(data);
+				var mensaje = {
+					idUser: usuario.id,
+					nombre: usuario.nombre,
+					mensaje: texto,
+					imagen: usuario.imagen
+				}
+				// console.log(mensaje);
+				nuevoMensaje(mensaje, db);
+				renderMensaje(data, "#mensajes");
+				$('#texto-mensaje').val('');
+			}
+		})
+		
+	}
+
+
+
+
+
+
+
+
+
+
 // Agregar mensajes a la base de datos.
-function nuevoMensaje(mensaje){
-	firebase.database().ref('mensajes/').push(mensaje);
+function nuevoMensaje(mensaje, db){
+	db.ref('mensajes/').push(mensaje);
 }
 
 
 // Función para agregar usuarios a la base de datos de firebase
-function agregarUsuario(user){
-	firebase.database().ref('usuarios/'+user.id).set(user);
+function agregarUsuario(user, db){
+	db.ref('usuarios/'+user.id).set(user);
 }
 
 // Función para conexión con Google
-function googleAuth(usuario){
+function googleAuth(usuario, db){
 	var google = new firebase.auth.GoogleAuthProvider();
 	    firebase.auth().signInWithPopup(google).then(function(result) {
 	      usuario.id = result.user.uid;
@@ -141,7 +143,7 @@ function googleAuth(usuario){
 		$('#numconect').attr('data-badge', conteo);
 // Fin
 	      renderUsers(usuario, "#usuarios");
-	      agregarUsuario(usuario);
+	      agregarUsuario(usuario, db);
 	    });
 // Conexión a la base de datos, agregando registros
 	return usuario;
